7b67b1990589c1c23d16ec68162fea375bc83539
Jason Storey <jason.andrew.storey@gmail.com>
2014-10-29 02:09:01 +0000

    Implement many 6502 opcodes

--- src/cpu.js
+++ src/cpu.js
@@ -1,9 +1,19 @@
 module.exports = CPU();
 
 function CPU() {
+
     var OPCODES = {
         '0': 'BRK',
-        '1': 'LDA'
+        '1': 'LDA',
+        '2': 'ADC',
+        '3': 'STA',
+        '4': 'LDX',
+        '5': 'INX',
+        '6': 'CMY',
+        '7': 'BNE',
+        '8': 'STA_X',
+        '9': 'DEY',
+        '10': 'LDY'
     };
 
     function createMemory(size) {
@@ -15,17 +25,28 @@ function CPU() {
     function create(memorySize) {
         var memory = createMemory(memorySize || 256),
             PC = 0,
-            registerA;
+            flag,
+            registerA,
+            registerX,
+            registerY;
 
         function getMemory() { return memory; }
 
         function getPC() { return PC; }
 
+        function getFlag() { return flag; }
+
         function getRegisterA() { return registerA; }
+
+        function getRegisterX() { return registerX; }
+
+        function getRegisterY() { return registerY; }
 
         function getCurrentOp() { return OPCODES[getCurrent()]; }
 
         function getCurrent() { return memory[PC]; }
+
+        function advance() { PC++; }
 
         function load(program) {
             if(program.length > memory.length) { throw new Error('Out of bounds error'); }
@@ -40,6 +61,50 @@ function CPU() {
                         registerA = getCurrent();
                         break;
 
+                    case 'ADC':
+                        advance();
+                        registerA += getCurrent();
+                        break;
+
+                    case 'STA':
+                        advance();
+                        memory[getCurrent()] = getRegisterA();
+                        break;
+
+                    case 'LDX':
+                        advance();
+                        registerX = getCurrent();
+                        break;
+
+                    case 'INX':
+                        registerX++;
+                        break;
+
+                    case 'CMY':
+                        advance();
+                        flag = (getCurrent() === getRegisterY());
+                        break;
+
+                    case 'BNE':
+                        advance();
+                        if(!getFlag()) {
+                            PC += getCurrent();
+                        }
+                        break;
+
+                    case 'STA_X':
+                        memory[getRegisterX()] = getRegisterA();
+                        break;
+
+                    case 'DEY':
+                        registerY--;
+                        break;
+
+                    case 'LDY':
+                        advance();
+                        registerY = getCurrent();
+                        break;
+
                     default :
                         throw new Error('Unrecognised OPCODE : ' + getCurrent());
                 }
@@ -47,14 +112,15 @@ function CPU() {
             }
         }
 
-        function advance() { PC++; }
-
         return {
             getMemory: getMemory,
             load: load,
             exec: exec,
             getPC: getPC,
-            getRegisterA: getRegisterA
+            getFlag: getFlag,
+            getRegisterA: getRegisterA,
+            getRegisterX: getRegisterX,
+            getRegisterY: getRegisterY
         };
     }
 
--- test/cpu-spec.js
+++ test/cpu-spec.js
@@ -58,11 +58,94 @@ describe('cpu', function() {
             expect(cpu.getPC()).to.equal(0);
         });
 
-        it('LDA stores current value in register A', function() {
+        it('LDA stores value from next memory address in register A', function() {
             cpu.load([1, 100, 0]);
             cpu.exec();
 
             expect(cpu.getRegisterA()).to.equal(100);
+            expect(cpu.getPC()).to.equal(2);
+        });
+
+        it('ADC adds value from next memory address to register A', function() {
+            cpu.load([1, 5, 2, 5, 0]);
+            cpu.exec();
+
+            expect(cpu.getRegisterA()).to.equal(10);
+            expect(cpu.getPC()).to.equal(4);
+        });
+
+        it('STA stores register A contents in memory location specified by value at next memory address', function() {
+            cpu.load([1, 5, 3, 2, 0]);
+            cpu.exec();
+
+            expect(cpu.getRegisterA()).to.equal(5);
+            expect(cpu.getMemory()[2]).to.equal(cpu.getRegisterA());
+            expect(cpu.getPC()).to.equal(4);
+        });
+
+        it('LDX stores value from next memory address in register X', function() {
+            cpu.load([4, 99, 0]);
+            cpu.exec();
+
+            expect(cpu.getRegisterX()).to.equal(99);
+            expect(cpu.getPC()).to.equal(2);
+        });
+
+        it('INX increment value in register X', function() {
+            cpu.load([4, 99, 5, 0]);
+            cpu.exec();
+
+            expect(cpu.getRegisterX()).to.equal(100);
+            expect(cpu.getPC()).to.equal(3);
+        });
+
+        it('CMY compares value from next memory address to registerY and sets boolean flag to true when same', function() {
+            cpu.load([10, 20, 6, 20, 0]);
+            cpu.exec();
+
+            expect(cpu.getRegisterY()).to.equal(20);
+            expect(cpu.getFlag()).to.be.true;
+            expect(cpu.getPC()).to.equal(4);
+        });
+
+        it('CMY compares value from next memory address to registerY and sets boolean flag to false when different', function() {
+            cpu.load([10, 20, 6, 21, 0]);
+            cpu.exec();
+
+            expect(cpu.getRegisterY()).to.equal(20);
+            expect(cpu.getFlag()).to.be.false;
+            expect(cpu.getPC()).to.equal(4);
+        });
+
+        it('BNE jumps program counter to value in next memory address if flag is false', function() {
+            cpu.load([10, 20, 6, 21, 7, 94, 0]);
+            cpu.exec();
+
+            expect(cpu.getFlag()).to.be.false;
+            expect(cpu.getPC()).to.equal(100);
+        });
+
+        it('STA_X stores register A contents in memory location specified by value in register X', function() {
+            cpu.load([1, 100, 4, 30, 8, 0]);
+            cpu.exec();
+
+            expect(cpu.getMemory()[30]).to.equal(100);
+            expect(cpu.getPC()).to.equal(5);
+        });
+
+        it('DEY decrement the value in register Y', function() {
+            cpu.load([10, 101, 9, 0]);
+            cpu.exec();
+
+            expect(cpu.getRegisterY()).to.equal(100);
+            expect(cpu.getPC()).to.equal(3);
+        });
+
+        it('LDY stores value from next memory address in register Y', function() {
+            cpu.load([10, 99, 0]);
+            cpu.exec();
+
+            expect(cpu.getRegisterY()).to.equal(99);
             expect(cpu.getPC()).to.equal(2);
         });
 
