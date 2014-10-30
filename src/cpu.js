4a28becc47d22b76b858b4c2da2c985a5a271424
Jason Storey <jason.andrew.storey@gmail.com>
2014-10-30 23:30:13 +0000

    Extract opcodes into separate module

--- src/cpu.js
+++ src/cpu.js
@@ -2,21 +2,7 @@ module.exports = CPU();
 
 function CPU() {
 
-    var OPCODES = {
-        '0': 'BRK',
-        '1': 'LDA',
-        '2': 'ADC',
-        '3': 'STA',
-        '4': 'LDX',
-        '5': 'INX',
-        '6': 'CMY',
-        '7': 'BNE',
-        '8': 'STA_X',
-        '9': 'DEY',
-        '10': 'LDY',
-        '11': 'JSR',
-        '12': 'RTS'
-    };
+    var OPCODES = require('./opcodes.js');
 
     function createMemory(size) {
         var memory = [];
