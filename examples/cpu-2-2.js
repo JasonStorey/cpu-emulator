3ab03a9be0e6025151978602feadbe7db3e2ffe6
Jason Storey <jason.andrew.storey@gmail.com>
2014-11-03 22:35:26 +0000

    Add cpu-2-2 dojo to examples

---
+++ examples/cpu-2-2.js
@@ -0,0 +1,23 @@
+var CPU = require('../src/cpu.js'),
+    Assembler = require('../src/assembler.js'),
+    fs = require('fs');
+
+var assembler = Assembler.create(),
+    cpu = CPU.create();
+
+var assembly = fs.readFileSync('../test/fixtures/cpu-app-2.txt', 'utf-8'),
+    program = assembler.parse(assembly);
+
+cpu.load(program);
+cpu.exec();
+
+printStringFromMemory(cpu.getMemory(), 128, 256);
+
+function printStringFromMemory(memory, s, e) {
+    var str = '';
+
+    memory.slice(s, e).forEach(function (charCode) {
+        str += String.fromCharCode(charCode);
+    });
+    console.log(str);
+}
